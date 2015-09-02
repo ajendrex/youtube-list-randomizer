3d89e0d7e098299c8e8a53cd172c96863decb628
Héctor Urbina <hurbina@buenavistacapital.com>
2015-09-02 12:57:44 -0300

    fix typo

--- readme.js
+++ readme.js
@@ -1,7 +1,7 @@
 Instructions:
 
 1. Go to your playlist's page (using firefox or chrome).
-2. Make sure to have all videos loades (clicking on "load more" button) if you want all videos randomized.
+2. Make sure to have all videos loaded (clicking on "load more" button) if you want all videos randomized.
 3. Open Console (ctrl+shift+i).
 4. Copy/paste the content of randomize.js on console and press enter.
 5. Good luck!
